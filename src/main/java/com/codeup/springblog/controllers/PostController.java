19ca43f6bdcb879a6672e8afb91b5b96f35ad23c
Kevin Orta <orta.kevin@gmail.com>
2022-01-06 09:03:50 -0600

    edit w/ delete progress

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 public class PostController {
@@ -16,9 +17,8 @@ public class PostController {
     }
 
     @GetMapping("/posts/show")
-    public String individualPosts(Model model) {
-//        Post newPost = new Post("Hello World", "Welcome to Codeup");
-//        model.addAttribute("post", newPost);
+    public String viewPosts(Model model) {
+        model.addAttribute("posts", postDao.findAll());
         return "posts/show";
     }
 
@@ -32,9 +32,14 @@ public class PostController {
         return "";
     }
 
-    public PostController(PostRepository postDao){
+    public PostController(PostRepository postDao) {
         this.postDao = postDao;
     }
 
-//    @GetMapping("delete/{id}")
+    @PostMapping("/deletebyid/{id}")
+    public String deletePostbyId(@PathVariable long id) {
+        postDao.deleteById(id);
+        return "redirect:/posts/show";
+    }
+
 }
